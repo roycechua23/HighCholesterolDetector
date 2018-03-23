b4c44df32a072b13bb422f4ace7c7bf4a8fea92a
roycechua23 <royce236@gmail.com>
2018-03-23 22:22:34 +0000

    Latest Major Update (Fixed)

--- RaspberryPi/EMBEDDED2FINAL2.py
+++ RaspberryPi/EMBEDDED2FINAL2.py
@@ -434,26 +434,6 @@ class acceptance(QWidget):
         self.qtRectangle.moveCenter(self.centerPoint)
         self.move(self.qtRectangle.topLeft())
 
-
-
-    # def cam(self):
-        
-    #     cap = cv2.VideoCapture(0)
-    #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    #     while(cap.isOpened()):
-    #         ret, frame = cap.read()
-    #         if ret==True:
-    #             cv2.imwrite('out.jpg', frame)
-    #             cv2.imshow('frame',frame)
-    #             if cv2.waitKey(1) & 0xFF == ord('q'):
-    #                 break   
-    #         else:
-    #             break
-
-    # # Release everything if job is finished
-    #     cap.release()
-    #     cv2.destroyAllWindows()
-
         self.initUI()
 
 
